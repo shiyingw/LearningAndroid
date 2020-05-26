9c1457a8f73ea439af5f841f09ee3ac36a926863
SHIYINGWU-01\shiyingwu <syw@xiangjiabao.com>
2020-05-26 15:27:46 +0800

    LearningAndroid: 学习 实现接口的方式 实现按钮的点击效果

--- app/src/main/java/com/syw/learningandroid/MainActivity.java
+++ app/src/main/java/com/syw/learningandroid/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import com.syw.learningandroid.camera.CameraFragment;
 import com.syw.learningandroid.clickeffect.ClickEffectFragment;
@@ -15,72 +14,69 @@ import com.syw.learningandroid.fragment.SingleFragmentActivity;
 import com.syw.learningandroid.permission.PermissionFragment;
 import com.syw.learningandroid.transferpara.TransferParaFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button buttonClickEffect = findViewById(R.id.btn_click_effect);
-        buttonClickEffect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openFragment(ClickEffectFragment.class.getName());
-            }
-        });
+        buttonClickEffect.setOnClickListener(this);
+
 
         Button buttonRequirePermission = findViewById(R.id.btn_require_permission);
-        buttonRequirePermission.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openFragment(PermissionFragment.class.getName());
-            }
-        });
+        buttonRequirePermission.setOnClickListener(this);
 
         Button buttonTransferPara = findViewById(R.id.btn_transfer_para);
-        buttonTransferPara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SingleFragmentActivity.class);
-                intent.putExtra(SingleFragmentActivity.FRAGMENT_NAME, TransferParaFragment.class.getName());
-                String friendBeanId = getFriendId();
-                // object to json
-                intent.putExtra("friend_bean_list_key", friendBeanId);
-                startActivity(intent);
-
-            }
-        });
+        buttonTransferPara.setOnClickListener(this);
 
         Button buttonCamera = findViewById(R.id.btn_open_camera);
-        buttonCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(CameraFragment.class.getName());
-            }
-        });
+        buttonCamera.setOnClickListener(this);
 
         Button buttonFragment = findViewById(R.id.btn_open_fragment);
-        buttonFragment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openFragment(LearningFragment.class.getName());
-            }
-        });
+        buttonFragment.setOnClickListener(this);
     }
 
     private String getFriendId() {
         return "18";
     }
 
-    private void openActivity(Class clazz){
-        Intent intent = new Intent(MainActivity.this, clazz);
-        startActivity(intent);
-    }
-
-    private void openFragment(String fragmentName){
-        Intent intent =new Intent(MainActivity.this, SingleFragmentActivity.class);
+    private void openFragment(String fragmentName) {
+        Intent intent = new Intent(MainActivity.this, SingleFragmentActivity.class);
         intent.putExtra(SingleFragmentActivity.FRAGMENT_NAME, fragmentName);
         startActivity(intent);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_click_effect:
+                openFragment(ClickEffectFragment.class.getName());
+                break;
+            case R.id.btn_require_permission:
+                openFragment(PermissionFragment.class.getName());
+                break;
+            case R.id.btn_transfer_para:
+                Intent intent = new Intent(MainActivity.this, SingleFragmentActivity.class);
+                intent.putExtra(SingleFragmentActivity.FRAGMENT_NAME, TransferParaFragment.class.getName());
+                String friendBeanId = getFriendId();
+                // object to json
+                intent.putExtra("friend_bean_list_key", friendBeanId);
+                startActivity(intent);
+                break;
+            case R.id.btn_open_camera:
+                openFragment(CameraFragment.class.getName());
+                break;
+            case R.id.btn_open_fragment:
+                openFragment(LearningFragment.class.getName());
+                break;
+            default:
+                break;
+
+        }
 
     }
 }
